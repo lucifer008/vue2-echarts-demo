10561538ae98fa889640ba831a5eaab269bd689f
lucifer008 <lucifer008@users.noreply.github.com>
2022-08-17 18:28:56 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+import ECharts from "vue-echarts";
+import "echarts";
+Vue.component("v-chart", ECharts);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App)
+}).$mount("#app");
